b3b9e19e2c5b9683346a51aa9fb977ed8a70b3c2
AkhRidwanYus <2010817210020@mhs.ulm.ac.id>
2022-04-25 16:40:02 +0800

    merapikan PRAK401.php

--- Modul4/PRAK401.php
+++ Modul4/PRAK401.php
@@ -17,57 +17,58 @@
 			Lebar : <input type="number" name="lebar" value="<?=isset($_POST["lebar"]) ? $_POST["lebar"] : ''?>"><br>
 			Nilai : <input type="text" name="nilai" value="<?=isset($_POST["nilai"]) ? $_POST["nilai"] : ''?>"><br>
 			<button type="submit" name="cetak">Cetak</button>
-			
-			<?php
-				if(isset($_POST["cetak"])){
-					echo "<br>";
-					if(isset($_POST["panjang"])){
-						$x = $_POST["panjang"];
-					}
-					if(isset($_POST["lebar"])){
-						$y = $_POST["lebar"];
-					}
-					$matriksize = $x * $y;
-					if(isset($_POST["nilai"])){
-						$nilai = explode(" ", $_POST["nilai"]);
-						$temp = count($nilai);
-						while($temp < $matriksize){
-							array_push($nilai, " ");
-							$temp ++;
-						}
-						$jumlah = count($nilai);
-					}
-					echo "<br>";
-					if($matriksize == $jumlah){
-						$i = 0;
-						$urutan = 0;
-						while($i < $y){
-							$j = 0;
-							while($j < $x){
-								$tampil[$j][$i] = $nilai[$urutan];
-								$urutan ++;
-								$j ++;
-								}
-							$i ++;
-						}
-						$i = 0;
-						echo "<table>";
-						while($i < $y){
-							$j = 0;
-							echo "<tr>";
-							while($j < $x){
-								$aa = $tampil[$j][$i];
-								echo "<td> $aa </td>";
-								$j ++;
-							}
-							echo "</tr>";
-							$i ++;
-						}
-						echo "</table>";
-					}
-					else{echo "Panjang nilai tidak sesuai dengan ukuran matriks";}
-				}
-			?>
 		</form>
+		<?php
+			if(isset($_POST["cetak"])){
+				$x = 0;
+				$y = 0;
+				echo "<br>";
+				if(isset($_POST["panjang"])){
+					$x = $_POST["panjang"];
+				}
+				if(isset($_POST["lebar"])){
+					$y = $_POST["lebar"];
+				}
+				$matriksize = $x * $y;
+				if(isset($_POST["nilai"])){
+					$nilai = explode(" ", $_POST["nilai"]);
+					$temp = count($nilai);
+					while($temp < $matriksize){
+						array_push($nilai, " ");
+						$temp ++;
+					}						
+					$jumlah = count($nilai);
+				}
+				echo "<br>";
+				if($matriksize == $jumlah){
+					$i = 0;
+					$urutan = 0;
+					while($i < $y){
+						$j = 0;
+						while($j < $x){
+							$tampil[$j][$i] = $nilai[$urutan];
+							$urutan ++;
+							$j ++;
+							}
+						$i ++;
+					}
+					$i = 0;
+					echo "<table>";
+					while($i < $y){
+						$j = 0;
+						echo "<tr>";
+						while($j < $x){
+							$aa = $tampil[$j][$i];
+							echo "<td> $aa </td>";
+							$j ++;
+						}
+						echo "</tr>";
+						$i ++;
+					}
+					echo "</table>";
+				}
+				else{echo "Panjang nilai tidak sesuai dengan ukuran matriks";}
+			}
+		?>
 	</body>
 </html>
