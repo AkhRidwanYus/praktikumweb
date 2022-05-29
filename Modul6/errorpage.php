1aa5d783c5cea78fb754136f0975197c34df3a6f
AkhRidwanYus <2010817210020@mhs.ulm.ac.id>
2022-05-29 13:34:55 +0800

    Update errorpage.php

--- Modul6/errorpage.php
+++ Modul6/errorpage.php
@@ -1,6 +1,6 @@
 <body>
 	<h3>Anda Tidak Memiliki Hak Akses Ke Halaman Ini!</h3>
-	<a href="index.php">Kembali Ke Halaman Utama</a>
+	<a href="index.php">Kembali Ke Halaman Utama</a><br>
 	<a href="member.php">Tabel Member</a><br>
 	<a href="buku.php">Tabel Buku</a><br>
 	<a href="peminjaman.php">Tabel Peminjaman</a><br>
